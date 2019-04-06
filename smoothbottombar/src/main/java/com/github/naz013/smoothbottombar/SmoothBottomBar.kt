5c9a74c81eb7bad9148151928e78a4be4a97571e
Nazar Suhovich <n.suhovich@gmail.com>
2019-04-06 10:16:08 +0300

    Add animations.

--- smoothbottombar/src/main/java/com/github/naz013/smoothbottombar/SmoothBottomBar.kt
+++ smoothbottombar/src/main/java/com/github/naz013/smoothbottombar/SmoothBottomBar.kt
@@ -16,6 +16,9 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.Nullable
 import androidx.annotation.Px
 import androidx.core.content.ContextCompat
+import androidx.dynamicanimation.animation.FloatPropertyCompat
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 
 class SmoothBottomBar : View {
 
@@ -31,7 +34,7 @@ class SmoothBottomBar : View {
     private val mSelectorPaint = Paint()
     private val mIconPaint = Paint()
 
-    private var mSelector: Selector = Selector(Point(0, 0), mSelectorPaint, Rect())
+    private var mSelector: Selector = Selector(PointF(0.0f, 0.0f), mSelectorPaint, Rect())
     private val objects: MutableList<InnerTab> = mutableListOf()
 
     private var mSelectedItem = 0
@@ -69,6 +72,79 @@ class SmoothBottomBar : View {
         setOnTouchListener { v, event -> processTouch(v, event) }
     }
 
+    override fun onDraw(canvas: Canvas?) {
+        if (canvas != null) {
+            canvas.drawColor(mBgColor)
+            mSelector.draw(canvas)
+            objects.forEach { it.draw(canvas) }
+        } else {
+            super.onDraw(canvas)
+        }
+    }
+
+    fun setTabs(tabs: List<Tab>) {
+        if (tabs.isEmpty()) {
+            throw IllegalArgumentException("Tabs cannot be empty")
+        }
+        this.mTabs = tabs
+        invalidate()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val height = View.MeasureSpec.getSize(heightMeasureSpec)
+        val width = View.MeasureSpec.getSize(widthMeasureSpec)
+        setMeasuredDimension(width, height)
+        calculateRectangles(width, height)
+    }
+
+    private fun calculateRectangles(width: Int, height: Int) {
+        val elements = mutableListOf<InnerTab>()
+        val margin = dp2px(16)
+        val tabWidth = (width - (margin * 2)) / mTabs.size
+        for (i in 0 until mTabs.size) {
+            val tab = mTabs[i]
+            val left = i * tabWidth + margin
+            val rect = Rect(left, margin, left + tabWidth, height - margin)
+            val leftHalf = Rect(rect.left, rect.top, rect.centerX(), rect.bottom)
+            val rightHalf = Rect(rect.centerX(), rect.top, rect.right, rect.bottom)
+            val label = Label(tab.title ?: "", 0, PointF(rightHalf.left.toFloat(), rightHalf.centerY().toFloat()), mTextPaint, rightHalf)
+            val iconHeight = rect.height() / 2
+            val iconHeightHalf = iconHeight / 2
+            val onPoint = PointF(leftHalf.centerX().toFloat() - iconHeightHalf, rect.centerY().toFloat() - iconHeightHalf)
+            val offPoint = PointF(rect.centerX().toFloat() - iconHeightHalf, rect.centerY().toFloat() - iconHeightHalf)
+            val iconBounds = Rect(0, 0, iconHeight, iconHeight)
+            val icon = Icon(onPoint, toDrawable(tab.icon), 255, offPoint, mIconPaint, iconBounds)
+            val innerTab = InnerTab(icon, label, rect)
+            elements.add(innerTab)
+            if (i == 0) {
+                mSelector = Selector(PointF(margin.toFloat(), margin.toFloat()), mSelectorPaint, rect)
+            }
+        }
+        objects.clear()
+        objects.addAll(elements)
+        if (mSelectedItem in 0..objects.size) {
+            selectTab(mSelectedItem)
+        }
+    }
+
+    @Nullable
+    override fun onSaveInstanceState(): Parcelable? {
+        val savedInstance = Bundle()
+        savedInstance.putParcelable("super", super.onSaveInstanceState())
+        savedInstance.putInt("position", mSelectedItem)
+        return savedInstance
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable) {
+        if (state is Bundle) {
+            mSelectedItem = state.getInt("position", mSelectedItem)
+            super.onRestoreInstanceState(state.getParcelable("super"))
+        } else {
+            super.onRestoreInstanceState(state)
+        }
+    }
+
     private fun processTouch(v: View, event: MotionEvent): Boolean {
         when {
             event.action == MotionEvent.ACTION_DOWN -> {
@@ -94,10 +170,10 @@ class SmoothBottomBar : View {
                         deSelectTab(mSelectedItem)
                         selectTab(item)
                         mSelectedItem = item
-                        invalidate()
-    //                    if (getOnTabSelectedListener() != null) {
-    //                        getOnTabSelectedListener().onTabSelected(item)
-    //                    }
+//                        invalidate()
+                        //                    if (getOnTabSelectedListener() != null) {
+                        //                        getOnTabSelectedListener().onTabSelected(item)
+                        //                    }
                     }
                     printLog("processTouch: up $item")
                     v.playSoundEffect(SoundEffectConstants.CLICK)
@@ -109,90 +185,13 @@ class SmoothBottomBar : View {
         }
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        if (canvas != null) {
-            canvas.drawColor(mBgColor)
-            mSelector.draw(canvas)
-            objects.forEach { it.draw(canvas) }
-        } else {
-            super.onDraw(canvas)
-        }
-    }
-
-    fun setTabs(tabs: List<Tab>) {
-        if (tabs.isEmpty()) {
-            throw IllegalArgumentException("Tabs cannot be empty")
-        }
-        this.mTabs = tabs
-        invalidate()
-    }
-
     private fun deSelectTab(position: Int) {
-        objects[position].label.alpha = 0
-        objects[position].icon.alpha = 155
-        objects[position].icon.point = objects[position].icon.defaultPoint
+        objects[position].animateOut()
     }
 
     private fun selectTab(position: Int) {
-        objects[position].label.alpha = 255
-        objects[position].icon.alpha = 255
-        objects[position].icon.point = objects[position].icon.onPoint
-        mSelector.point = objects[position].defaultPoint
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val height = View.MeasureSpec.getSize(heightMeasureSpec)
-        val width = View.MeasureSpec.getSize(widthMeasureSpec)
-        setMeasuredDimension(width, height)
-        calculateRectangles(width, height)
-    }
-
-    private fun calculateRectangles(width: Int, height: Int) {
-        val elements = mutableListOf<InnerTab>()
-        val margin = dp2px(16)
-        val tabWidth = (width - (margin * 2)) / mTabs.size
-        for (i in 0 until mTabs.size) {
-            val tab = mTabs[i]
-            val left = i * tabWidth + margin
-            val rect = Rect(left, margin, left + tabWidth, height - margin)
-            val leftHalf = Rect(rect.left, rect.top, rect.centerX(), rect.bottom)
-            val rightHalf = Rect(rect.centerX(), rect.top, rect.right, rect.bottom)
-            val label = Label(tab.title ?: "", 0, Point(rightHalf.left, rightHalf.centerY()), mTextPaint, rightHalf)
-            val iconHeight = rect.height() / 2
-            val iconHeightHalf = iconHeight / 2
-            val onPoint = Point(leftHalf.centerX() - iconHeightHalf, rect.centerY() - iconHeightHalf)
-            val offPoint = Point(rect.centerX() - iconHeightHalf, rect.centerY() - iconHeightHalf)
-            val iconBounds = Rect(0, 0, iconHeight, iconHeight)
-            val icon = Icon(onPoint, toDrawable(tab.icon), 255, offPoint, mIconPaint, iconBounds)
-            val innerTab = InnerTab(icon, label, rect)
-            elements.add(innerTab)
-            if (i == 0) {
-                mSelector = Selector(Point(margin, margin), mSelectorPaint, rect)
-            }
-        }
-        objects.clear()
-        objects.addAll(elements)
-        if (mSelectedItem in 0..objects.size) {
-            selectTab(mSelectedItem)
-        }
-    }
-
-    @Nullable
-    override fun onSaveInstanceState(): Parcelable? {
-        val savedInstance = Bundle()
-        savedInstance.putParcelable("super", super.onSaveInstanceState())
-        savedInstance.putInt("position", mSelectedItem)
-        return savedInstance
-    }
-
-    override fun onRestoreInstanceState(state: Parcelable) {
-        if (state is Bundle) {
-            mSelectedItem = state.getInt("position", mSelectedItem)
-            super.onRestoreInstanceState(state.getParcelable("super"))
-        } else {
-            super.onRestoreInstanceState(state)
-        }
+        mSelector.animateTo(objects[position].point)
+        objects[position].animateIn()
     }
 
     private fun findIndex(x: Float, y: Float): Int {
@@ -234,36 +233,107 @@ class SmoothBottomBar : View {
         Log.d("SmoothBottomBar", message)
     }
 
-    private fun toRect(rect: Rect, point: Point): Rect {
-        return Rect(point.x, point.y, point.x + rect.width(), point.y + rect.height())
+    private fun toRect(rect: Rect, point: PointF): Rect {
+        return Rect(point.x.toInt(), point.y.toInt(), point.x.toInt() + rect.width(), point.y.toInt() + rect.height())
     }
 
-    private fun toRectF(rect: Rect, point: Point): RectF {
+    private fun toRectF(rect: Rect, point: PointF): RectF {
         return RectF(
-            point.x.toFloat(), point.y.toFloat(),
-            point.x.toFloat() + rect.width().toFloat(), point.y.toFloat() + rect.height().toFloat()
+            point.x, point.y,
+            point.x + rect.width().toFloat(), point.y + rect.height().toFloat()
         )
     }
 
-    inner class InnerTab(var icon: Icon, var label: Label, bounds: Rect) : PaintObject(Point(bounds.left, bounds.top), Paint(), bounds) {
+    inner class InnerTab(var icon: Icon, var label: Label, bounds: Rect)
+        : PaintObject(PointF(bounds.left.toFloat(), bounds.top.toFloat()), Paint(), bounds) {
         override fun draw(canvas: Canvas) {
             icon.draw(canvas)
             label.draw(canvas)
         }
+
+        fun animateIn() {
+            icon.animateIn()
+            label.animateIn()
+        }
+
+        fun animateOut() {
+            icon.animateOut()
+            label.animateOut()
+        }
     }
 
-    inner class Icon(val onPoint: Point, private val drawable: Bitmap?, var alpha: Int = 255, defaultPoint: Point, paint: Paint, bounds: Rect) :
-        PaintObject(defaultPoint, paint, bounds) {
+    inner class Icon(private val onPoint: PointF, private val drawable: Bitmap?, var alpha: Int = 255,
+                     defaultPoint: PointF, paint: Paint, bounds: Rect) : PaintObject(defaultPoint, paint, bounds) {
+
+        private val alphaPropertyAnim = object : FloatPropertyCompat<Icon>("icon_alpha") {
+            override fun getValue(icon: Icon?): Float {
+                return icon?.alpha?.toFloat() ?: alpha.toFloat()
+            }
+
+            override fun setValue(icon: Icon?, value: Float) {
+                icon?.alpha = value.toInt()
+                invalidate()
+            }
+        }
+
+        private val translatePropertyAnimX = object : FloatPropertyCompat<Icon>("icon_x") {
+            override fun setValue(icon: Icon?, value: Float) {
+                icon?.point = PointF(value, point.y)
+                invalidate()
+            }
+            override fun getValue(icon: Icon?): Float {
+                return icon?.point?.x ?: defaultPoint.x
+            }
+        }
+
         override fun draw(canvas: Canvas) {
             if (drawable != null) {
                 paint.alpha = alpha
                 canvas.drawBitmap(drawable, null, toRect(bounds, point), paint)
             }
         }
+
+        fun animateIn() {
+            SpringAnimation(this, translatePropertyAnimX, onPoint.x).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
+            SpringAnimation(this, alphaPropertyAnim, 255f).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
+        }
+
+        fun animateOut() {
+            SpringAnimation(this, translatePropertyAnimX, defaultPoint.x).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
+            SpringAnimation(this, alphaPropertyAnim, 123f).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
+        }
     }
 
-    inner class Label(private var text: String, var alpha: Int = 0, defaultPoint: Point, paint: Paint, bounds: Rect) :
+    inner class Label(private var text: String, var alpha: Int = 0, defaultPoint: PointF, paint: Paint, bounds: Rect) :
         PaintObject(defaultPoint, paint, bounds) {
+
+        private val alphaPropertyAnim = object : FloatPropertyCompat<Label>("label_alpha") {
+            override fun getValue(label: Label?): Float {
+                return label?.alpha?.toFloat() ?: alpha.toFloat()
+            }
+
+            override fun setValue(label: Label?, value: Float) {
+                label?.alpha = value.toInt()
+                invalidate()
+            }
+        }
+
         override fun draw(canvas: Canvas) {
             val r = Rect()
             paint.alpha = alpha
@@ -274,19 +344,53 @@ class SmoothBottomBar : View {
                 mTextPaint.textSize = mTextPaint.textSize * fac
             }
 
-            canvas.drawText(text, defaultPoint.x.toFloat(), bounds.centerY() - r.exactCenterY(), paint)
+            canvas.drawText(text, defaultPoint.x, bounds.centerY() - r.exactCenterY(), paint)
+        }
+
+        fun animateIn() {
+            SpringAnimation(this, alphaPropertyAnim, 255f).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
+        }
+
+        fun animateOut() {
+            SpringAnimation(this, alphaPropertyAnim, 0f).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
         }
     }
 
-    inner class Selector(defaultPoint: Point, paint: Paint, bounds: Rect) : PaintObject(defaultPoint, paint, bounds) {
+    inner class Selector(defaultPoint: PointF, paint: Paint, bounds: Rect) : PaintObject(defaultPoint, paint, bounds) {
         private val cornersRadius = dp2px(5).toFloat()
+        private val floatPropertyAnimX = object : FloatPropertyCompat<Selector>("selector_x") {
+            override fun setValue(selector: Selector?, value: Float) {
+                selector?.point = PointF(value, point.y)
+                invalidate()
+            }
+            override fun getValue(selector: Selector?): Float {
+                return selector?.point?.x ?: defaultPoint.x
+            }
+        }
+
+        fun animateTo(point: PointF) {
+            SpringAnimation(this, floatPropertyAnimX, point.x).apply {
+                spring.stiffness = SpringForce.STIFFNESS_LOW
+                spring.dampingRatio = SpringForce.DAMPING_RATIO_NO_BOUNCY
+                start()
+            }
+        }
+
         override fun draw(canvas: Canvas) {
             canvas.drawRoundRect(toRectF(bounds, point), cornersRadius, cornersRadius, paint)
         }
     }
 
-    abstract inner class PaintObject(val defaultPoint: Point, protected val paint: Paint, val bounds: Rect) {
-        var point: Point = Point(defaultPoint.x, defaultPoint.y)
+    abstract inner class PaintObject(val defaultPoint: PointF, protected val paint: Paint, val bounds: Rect) {
+        var point: PointF = PointF(defaultPoint.x, defaultPoint.y)
         abstract fun draw(canvas: Canvas)
     }
 
